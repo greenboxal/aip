7685e8ccd548efcd18d7d0ed2a4624861ea2f496
Jonathan Lima <greenboxal@gmail.com>
2023-05-12 15:18:59 -0400

    z

--- web/src/resources/Span.tsx
+++ web/src/resources/Span.tsx
@@ -1,36 +1,66 @@
 import React from 'react'
 
-import {ChipField, DatagridConfigurable, DateField, ReferenceArrayField, ReferenceField, SelectColumnsButton, Show, SimpleShowLayout, TextField, TopToolbar,} from "react-admin"
+import {
+    ChipField,
+    Datagrid,
+    DatagridConfigurable,
+    DateField,
+    ReferenceArrayField,
+    ReferenceField,
+    SelectColumnsButton,
+    Show,
+    SimpleShowLayout,
+    TextField,
+    TopToolbar, WrapperField,
+} from "react-admin"
 
-import {ListLive} from '@react-admin/ra-realtime';
+import {ListLive, ShowLive} from '@react-admin/ra-realtime';
 import {MarkdownField} from "@react-admin/ra-markdown";
 
 export const SpanListActions = () => (<TopToolbar>
     <SelectColumnsButton/>
 </TopToolbar>)
 
+export const SpanListDatagrid: React.FC<{
+    includeTraceId?: boolean,
+}> = (props) => (
+    <Datagrid rowClick="show" size="small">
+        <TextField source="metadata.id" label="ID"/>
+
+        { props.includeTraceId ? (<ReferenceField source="trace_id" reference="Trace" label="Trace ID" link="show" />) : null }
+
+        <TextField source="name" label="Name"/>
+
+        <DateField source="started_at" label="Started At" />
+        <DateField source="completed_at" label="Completed At" />
+    </Datagrid>
+)
+
 export const SpanList = () => (
     <ListLive actions={<SpanListActions/>} sort={{ field: 'metadata.created_at', order: 'DESC' }}>
-        <DatagridConfigurable rowClick="show" size="small" preferenceKey="spans.datagrid">
-            <TextField source="id" label="ID"/>
-            <TextField source="name" label="Name"/>
-            <DateField source="metadata.started_at" label="Started At" />
-            <DateField source="metadata.completed_at" label="Completed At" />
-        </DatagridConfigurable>
+        <SpanListDatagrid includeTraceId={true} />
     </ListLive>
 )
 
 export const SpanShow = () => (
-    <Show>
+    <ShowLive>
         <SimpleShowLayout>
             <TextField source="id" label="ID"/>
             <TextField source="name" label="Name"/>
-            <DateField source="metadata.started_at" label="Started At" />
-            <DateField source="metadata.completed_at" label="Completed At" />
+            <DateField source="started_at" label="Started At" />
+            <DateField source="completed_at" label="Completed At" />
 
             <ReferenceField source="trace_id" reference="Trace" label="Trace" link="show">
                 <ChipField source="metadata.id"/>
             </ReferenceField>
+
+            <ReferenceField reference="Span" source="parent_id" label="Parent" link="show">
+                <ChipField source="metadata.id"/>
+            </ReferenceField>
+
+            <ReferenceArrayField reference="Span" source="inner_span_ids">
+                <SpanListDatagrid />
+            </ReferenceArrayField>
         </SimpleShowLayout>
-    </Show>
+    </ShowLive>
 );
--- web/src/resources/Trace.tsx
+++ web/src/resources/Trace.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-import {Datagrid, DatagridConfigurable, DateField, ReferenceArrayField, SelectColumnsButton, Show, SimpleShowLayout, TextField, TopToolbar,} from "react-admin"
+import {
+    ChipField,
+    Datagrid,
+    DatagridConfigurable,
+    DateField,
+    ReferenceArrayField,
+    ReferenceField,
+    SelectColumnsButton,
+    Show,
+    SimpleShowLayout,
+    TextField,
+    TopToolbar,
+} from "react-admin"
 
-import {ListLive} from '@react-admin/ra-realtime';
+import {ListLive, ShowLive} from '@react-admin/ra-realtime';
 import {MarkdownField} from "@react-admin/ra-markdown";
-import {SpanShow} from "./Span";
+import {SpanListDatagrid, SpanShow} from "./Span";
 
 export const TraceListActions = () => (<TopToolbar>
     <SelectColumnsButton/>
@@ -19,18 +31,28 @@ export const TraceList = () => (
 )
 
 export const TraceShow = () => (
-    <Show>
+    <ShowLive>
         <SimpleShowLayout>
             <TextField source="id" label="ID"/>
 
-            <ReferenceArrayField reference="Span" source="spans" label="Spans">
-                <Datagrid rowClick="show" size="small">
-                    <TextField source="id" label="ID"/>
-                    <TextField source="name" label="Name"/>
-                    <DateField source="metadata.started_at" label="Started At" />
-                    <DateField source="metadata.completed_at" label="Completed At" />
-                </Datagrid>
-            </ReferenceArrayField>
+            <ReferenceField reference="Span" source="root_span_id" label="Root Span">
+                <TextField source="id" label="ID"/>
+                <TextField source="name" label="Name"/>
+                <DateField source="started_at" label="Started At" />
+                <DateField source="completed_at" label="Completed At" />
+
+                <ReferenceField source="trace_id" reference="Trace" label="Trace" link="show">
+                    <ChipField source="metadata.id"/>
+                </ReferenceField>
+
+                <ReferenceField reference="Span" source="parent_id" label="Parent" link="show">
+                    <ChipField source="metadata.id"/>
+                </ReferenceField>
+
+                <ReferenceArrayField reference="Span" source="inner_span_ids">
+                    <SpanListDatagrid />
+                </ReferenceArrayField>
+            </ReferenceField>
         </SimpleShowLayout>
-    </Show>
+    </ShowLive>
 );
